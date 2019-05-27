c8b4efb2da9e3f3548fe367b85a75df1f7fc739e
Wilson Guiovanny Muñoz Daza <wilson176@unicauca.edu.co>
2019-05-27 09:07:14 -0500

    Development of Match Activity

--- app/src/main/java/com/example/appcup/MainActivity.java
+++ app/src/main/java/com/example/appcup/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     Button positionsButton, matchesButton, searchPlayerButton, newsButton;
+    private ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +32,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(positions);
             }
         });
+
+        matchesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent match= new Intent(MainActivity.this, MatchActivity.class);
+                startActivity(match);
+            }
+        });
+
+        searchPlayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent search= new Intent(MainActivity.this, SearchPlayerActivity.class);
+                startActivity(search);
+            }
+        });
+
+        newsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent news= new Intent(MainActivity.this, NewsActivity.class);
+                startActivity(news);
+            }
+        });
     }
 
     //Create the options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.logomenu, menu);
